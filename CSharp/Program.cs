e20a96240e09f8c70c4209f290ca37a507504250
Rubén Gonzalo Soria Soria <rusok.trabajo@hotmail.com>
2023-05-29 19:50:06 -0400

    Introducción a variables

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            /*
+             ***************************************
+             * PROYECTO open source GNU
+             * 
+             * Autor: Pepe
+             ***************************************
+             */
+            int ladoX = 2;
+            int ladoY = 4;
+            int radio = 5;
+
+            int potencia = 9;
+
+            int cant_quinua = 34;
+
+            //carnet de identidad
+            int ci = 9876543;
+            //numero de identificación tributaria
+            int nit = 987654016;
+
+            float peso = 12.0f;
+
+            double gravedad = 9.8;
+
+            double micro = peso;
+
+            float x2 = 3.1416f;
+
+            Console.WriteLine(x2);
+
+            int res = (int)x2;
+            Console.WriteLine(res);
+
+            Console.WriteLine("¡Hola mundo!");
         }
     }
 }
